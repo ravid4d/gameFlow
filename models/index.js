4bf5a71ac76d2f596b8e165f43741265ccc26b26
ravid4d <ravichaudharyd4d@gmail.com>
2025-07-15 18:59:19 +0530

    minor changes

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_CONNECTION,
+  dialect: "mysql",
 });
 
 export default sequelize;
