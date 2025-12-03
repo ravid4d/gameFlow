aaa5f5e452466c4799627f2b7a05873c791d7dba
ravid4d <ravichaudharyd4d@gmail.com>
2025-12-03 16:57:17 +0530

    added try catch in services functions

--- services/gameService.js
+++ services/gameService.js
@@ -3,167 +3,258 @@ import { Sequelize } from 'sequelize'
 import ScratchGame from '../models/scratch_game.js'
 import User from '../models/user.js'
 import ScratchGameMove from '../models/scratch_game_move.js'
+import e from 'express'
 
 const processMove = async (gameId, box_number, userId) => {
-  return await sequelize.transaction(async t => {
-    console.log('Processing move:', { gameId, box_number, userId })
-    const game = await ScratchGame.findByPk(gameId, { transaction: t })
-    if (!game) {
-      throw new Error('Game not found')
-    }
+  try {
+    return await sequelize.transaction(async t => {
+      console.log('Processing move:', { gameId, box_number, userId })
+      const game = await ScratchGame.findByPk(gameId, { transaction: t })
+      if (!game) {
+        return {
+          success: false,
+          message: 'Game not found'
+        }
+      }
 
-    const savedMove = await ScratchGameMove.create(
-      {
-        game_id: gameId,
-        user_id: userId,
-        box_number: box_number,
-        is_winner: is_winner || false
-      },
-      { transaction: t }
-    )
-    const is_winner = box_number === game.winner_box_number
-    if (!is_winner) {
-      return {
-        move: {
-          gameId,
-          box_number,
-          userId,
-          moveId: savedMove.id
+      const is_winner = box_number === game.winner_box_number
+      const savedMove = await ScratchGameMove.create(
+        {
+          game_id: gameId,
+          user_id: userId,
+          box_number: box_number,
+          is_winner: is_winner || false
         },
-        gameFinished: false
+        { transaction: t }
+      )
+
+      if (!is_winner) {
+        return {
+          success: true,
+          message: 'Move recorded, game continues',
+          data: {
+            gameFinished: false,
+            move: {
+              gameId,
+              box_number,
+              userId,
+              moveId: savedMove.id
+            }
+          }
+        }
       }
-    }
-    const winnerId = userId
-    const loserId = game.user1_id === userId ? game.user2_id : game.user1_id
+      const winnerId = userId
+      const loserId = game.user1_id === userId ? game.user2_id : game.user1_id
 
-    const entryFee = parseFloat(game.game_amount)
-    const winner = await User.findByPk(winnerId, { transaction: t })
-    const loser = await User.findByPk(loserId, { transaction: t })
-    const loserBalance = parseFloat(loser.balance)
-    const winnerBalance = parseFloat(winner.balance)
+      const entryFee = parseFloat(game.game_amount)
+      const winner = await User.findByPk(winnerId, { transaction: t })
+      const loser = await User.findByPk(loserId, { transaction: t })
+      const loserBalance = parseFloat(loser.balance)
+      const winnerBalance = parseFloat(winner.balance)
 
-    if (loserBalance < entryFee) {
-      throw new Error('Loser has insufficient balance')
-    }
+      if (loserBalance < entryFee) {
+        return {
+          success: false,
+          message: 'Loser has insufficient balance'
+        }
+        // throw new Error('Loser has insufficient balance')
+      }
 
-    loser.balance = (loserBalance - entryFee).toFixed(2)
-    winner.balance = (winnerBalance + entryFee).toFixed(2)
+      loser.balance = (loserBalance - entryFee).toFixed(2)
+      winner.balance = (winnerBalance + entryFee).toFixed(2)
 
-    await loser.save({ transaction: t })
-    await winner.save({ transaction: t })
+      await loser.save({ transaction: t })
+      await winner.save({ transaction: t })
 
-    game.status = 'completed'
-    game.winner_id = winnerId
-    await game.save({ transaction: t })
+      game.status = 'completed'
+      game.winner_id = winnerId
+      await game.save({ transaction: t })
 
+      return {
+        success: true,
+        message: `User ${winner.first_name} won the game and received ${entryFee} units.`,
+        data: {
+          gameFinished: true,
+          gameResult: {
+            winnerId,
+            loserId,
+            message :  `User ${winner.first_name} won the game and received ${entryFee} units.`
+          },
+          move: {
+            gameId,
+            box_number,
+            userId,
+            moveId: savedMove.id
+          }
+        }
+      }
+    })
+  } catch (error) {
+    console.error('Error in processMove:', error)
     return {
-      move: {
-        gameId,
-        box_number,
-        userId,
-        moveId: savedMove.id
-      },
-      gameFinished: true,
-      gameResult: {
-        winnerId,
-        loserId,
-        message: `User ${winner.first_name} won the game and received ${entryFee} units.`
-      }
+      success: false,
+      message: error.message
     }
-  })
+    //throw error
+  }
 }
 const leaveGameRoom = async (gameId, userId) => {
-  return await sequelize.transaction(async t => {
-    const game = await ScratchGame.findByPk(gameId, { transaction: t })
-    if (!game) {
-      throw new Error('Game not found')
-    }
-
-    const loserId = userId
-    const winnerId = game.user1_id === userId ? game.user2_id : game.user1_id
-
-    const entryFee = parseFloat(game.game_amount)
-    const winner = await User.findByPk(winnerId, { transaction: t })
-    const loser = await User.findByPk(loserId, { transaction: t })
-
-    const loserBalance = parseFloat(loser.balance)
-    const winnerBalance = parseFloat(winner.balance)
-    if (loserBalance < entryFee) {
-      throw new Error('Loser has insufficient balance')
-    }
-
-    loser.balance = (loserBalance - entryFee).toFixed(2)
-    winner.balance = (winnerBalance + entryFee).toFixed(2)
-
-    await loser.save({ transaction: t })
-    await winner.save({ transaction: t })
-
-    game.status = 'leaved'
-    game.leave_user_id = userId
-    game.winner_id = winnerId
-    await game.save({ transaction: t })
-    return {
-      gameFinished: true,
-      gameResult: {
-        winnerId,
-        loserId,
-        message: `User ${loser.first_name} left the game ${gameId}. Game marked as leaved.`
+  try {
+    return await sequelize.transaction(async t => {
+      const game = await ScratchGame.findByPk(gameId, { transaction: t })
+      if (!game) {
+        return {
+          success: false,
+          message: 'Game not found'
+        }
+        // throw new Error('Game not found')
       }
+
+      const loserId = userId
+      const winnerId = game.user1_id === userId ? game.user2_id : game.user1_id
+
+      const entryFee = parseFloat(game.game_amount)
+      const winner = await User.findByPk(winnerId, { transaction: t })
+      const loser = await User.findByPk(loserId, { transaction: t })
+
+      const loserBalance = parseFloat(loser.balance)
+      const winnerBalance = parseFloat(winner.balance)
+      if (loserBalance < entryFee) {
+        return {
+          success: false,
+          message: 'Loser has insufficient balance'
+        }
+        // throw new Error('Loser has insufficient balance')
+      }
+
+      loser.balance = (loserBalance - entryFee).toFixed(2)
+      winner.balance = (winnerBalance + entryFee).toFixed(2)
+
+      await loser.save({ transaction: t })
+      await winner.save({ transaction: t })
+
+      game.status = 'leaved'
+      game.leave_user_id = userId
+      game.winner_id = winnerId
+      await game.save({ transaction: t })
+      return {
+        success: true,
+        message: `User ${winner.first_name} won the game as User ${loser.first_name} left and received ${entryFee} units.`,
+        data: {
+          gameFinished: true,
+          gameResult: {
+            winnerId,
+            loserId,
+            message : `User ${winner.first_name} won the game as User ${loser.first_name} left and received ${entryFee} units.`
+          }
+        }
+      }
+    })
+  } catch (error) {
+    console.error('Error in leaveGameRoom:', error)
+    return {
+      success: false,
+      message: error.message
     }
-  })
+    // throw error
+  }
 }
 
 const cancelGame = async (gameId, userId) => {
-  return await sequelize.transaction(async t => {
-    const game = await ScratchGame.findByPk(gameId, { transaction: t })
-    if (!game) {
-      throw new Error('Game not found')
-    }
-    if (game.user1_id !== userId) {
-      throw new Error('Only the game creator can cancel the game');
-    }
-    game.status = 'cancelled'
-    await game.save({ transaction: t })
-    return {
-      gameFinished: true,
-      gameResult: {
-        message: `User ${userId} cancelled the game. Game marked as cancelled.`
+  try {
+    return await sequelize.transaction(async t => {
+      const game = await ScratchGame.findByPk(gameId, { transaction: t })
+      if (!game) {
+        return {
+          success: false,
+          message: 'Game not found'
+        }
+        // throw new Error('Game not found')
       }
+      if (game.user1_id !== userId) {
+        return {
+          success: false,
+          message: 'Only the game creator can cancel the game'
+        }
+        // throw new Error('Only the game creator can cancel the game')
+      }
+      game.status = 'cancelled'
+      await game.save({ transaction: t })
+      return {
+        success: true,
+        message: 'Game is cancelled by the owner',
+        data: {
+          gameFinished: true,
+          message : 'Game is cancelled by the owner'
+        }
+      }
+    })
+  } catch (error) {
+    console.error('Error in cancelGame:', error)
+    return {
+      success: false,
+      message: error.message
     }
-  });
+    // throw error
+  }
 }
 
-const startGame = async (gameId,userId) => {
-  return await sequelize.transaction(async t => {
-    const game = await ScratchGame.findByPk(gameId, { transaction: t })
-    if (!game) {
-      throw new Error('Game not found')
-    }
-    if(game.user1_id !== userId) {
-      throw new Error('Only the game creator can start the game');
-    }
-
-    if(!game.user2_id) {
-      throw new Error('Cannot start game without a second player');
-    }
-
-    const players = [game.user1_id, game.user2_id];
-    const firstTurn = players[Math.floor(Math.random() * players.length)];
-
-    const firstTurnUser = await User.findByPk(firstTurn, { transaction: t });
-
-    game.status = 'active';
-    await game.save({ transaction: t });
-
-    return {
-      gameResult: {
-        success: true,
-        gameId,
-        firstTurn,
-        message: `Game ${gameId} started. User ${firstTurnUser.first_name} will make the first move.`
+const startGame = async (gameId, userId) => {
+  try {
+    return await sequelize.transaction(async t => {
+      const game = await ScratchGame.findByPk(gameId, { transaction: t })
+      if (!game) {
+        return {
+          success: false,
+          message: 'Game not found'
+        }
+        //throw new Error('Game not found')
       }
+      if (game.user1_id !== userId) {
+        return {
+          success: false,
+          message: 'Only the game creator can start the game'
+        }
+        // throw new Error('Only the game creator can start the game')
+      }
+
+      if (!game.user2_id) {
+        return {
+          success: false,
+          message: 'Cannot start game without a second player'
+        }
+        // throw new Error('Cannot start game without a second player')
+      }
+
+      const players = [game.user1_id, game.user2_id];
+      const firstTurn = players[Math.floor(Math.random() * players.length)];
+
+      const firstTurnUser = await User.findByPk(firstTurn, { transaction: t });
+
+      game.status = 'active';
+      await game.save({ transaction: t });
+
+      return {
+        success: true,
+        message: `Game ${gameId} started. User ${firstTurnUser.first_name} will make the first move.`,
+        data: {
+          gameId,
+          firstTurn,
+          message : `Game ${gameId} started. User ${firstTurnUser.first_name} will make the first move.`,
+          firstTurnUser: {
+            id: firstTurnUser.id,
+            name: firstTurnUser.first_name
+          }
+        }
+      };
+    })
+  } catch (error) {
+    console.error('Error in startGame:', error)
+    return {
+      success: false,
+      message: error.message
     }
-  });
-};
+  }
+}
 
 export default { processMove, leaveGameRoom, cancelGame, startGame }
--- sockets/gameSocket.js
+++ sockets/gameSocket.js
@@ -3,64 +3,84 @@ import gameService from '../services/gameService.js'
 
 const gameSocket = (io, socket) => {
   socket.on('joinGameRoom', gameId => {
-    const roomName = `game_${gameId}`
-    socket.join(roomName)
-    console.log(`Socket ${socket.id} joined room ${roomName}`)
+    const roomName = `game_${gameId}`;
+    socket.join(roomName);
+    console.log(`Socket ${socket.id} joined room ${roomName}`);
   })
 
-  socket.on('startGame', data => {
-    const roomName = `game_${data.gameId}`;
-    const result = gameService.startGame(data.gameId);
-    io.to(roomName).emit('gameStarted',result.gameResult);
-    console.log(`Game started in room ${roomName}`)
-  });
-  
+  socket.on('startGame', async data => {
+    try {
+      const roomName = `game_${data.gameId}`;
+      const result = await gameService.startGame(data.gameId, data.userId);
+      if (!result.success) {
+        socket.emit('startGameError', { message: result.message });
+        return;
+      }
+      io.to(roomName).emit('gameStarted', result.data);
+      console.log(`Game started in room ${roomName}`);
+    } catch (error) {
+      console.error('Error starting game:', error);
+      socket.emit('startGameError', { message: 'Failed to start game' });
+    }
+  })
+
   socket.on('playerMove', async data => {
     try {
       const { gameId, box_number, userId } = data
-      const result = await gameService.processMove(gameId, box_number, userId)
-
-      console.log(`Move saved for game ${gameId}`)
-      console.log(gameId)
+      const result = await gameService.processMove(gameId, box_number, userId);
+      if (!result.success) {
+        socket.emit('playerMoveError', { message: result.message });
+        return;
+      }
+      console.log(`Move saved for game ${gameId}`);
+      console.log(gameId);
       // 📡 Notify both players in the same room
-      io.to(`game_${data.gameId}`).emit('moveMade', result.move)
+      io.to(`game_${data.gameId}`).emit('moveMade', result.data.move);
 
-      if (result.gameFinished) {
-        io.to(`game_${data.gameId}`).emit('gameOver', result.gameResult)
+      if (result.data.gameFinished) {
+        io.to(`game_${data.gameId}`).emit('gameOver', result.data.gameResult);
       }
     } catch (error) {
-      console.error('Error saving move:', error)
-      socket.emit('error', { message: 'Failed to save move' })
+      console.error('Error saving move:', error);
+      socket.emit('playerMoveError', { message: 'Failed to save move' });
     }
-  });
+  })
 
   socket.on('leaveGameRoom', async data => {
     try {
-      const { gameId, userId } = data
-      const roomName = `game_${data.gameId}`
-      const result = await gameService.leaveGameRoom(gameId, userId)
-      io.to(`game_${data.gameId}`).emit('gameOver', result.gameResult)
-      socket.leave(roomName)
-      console.log(`Socket ${socket.id} left room ${roomName}`)
+      const { gameId, userId } = data;
+      const roomName = `game_${data.gameId}`;
+      const result = await gameService.leaveGameRoom(gameId, userId);
+      if (!result.success) {
+        socket.emit('leaveGameRoomError', { message: result.message });
+        return;
+      }
+      io.to(`game_${data.gameId}`).emit('gameOver', result.data.gameResult);
+      socket.leave(roomName);
+      console.log(`Socket ${socket.id} left room ${roomName}`);
     } catch (error) {
-      console.error('Error leaving game room:', error)
-      socket.emit('error', { message: 'Failed to leave game room' })
+      console.error('Error leaving game room:', error);
+      socket.emit('leaveGameRoomError', {message: 'Failed to leave game room'});
     }
-  })
+  });
 
   socket.on('cancelGame', async data => {
     try {
       const { gameId, userId } = data
       const roomName = `game_${data.gameId}`
-      const result = await gameService.cancelGame(gameId, userId);
-      io.to(`game_${data.gameId}`).emit('gameCancelled', result.gameResult);
+      const result = await gameService.cancelGame(gameId, userId)
+      if (!result.success) {
+        socket.emit('cancelGameError', { message: result.message });
+        return;
+      }
+      io.to(`game_${data.gameId}`).emit('gameCancelled', result.data);
       socket.leave(roomName);
-      console.log(`Socket ${socket.id} left room ${roomName}`);
+      console.log(`Socket ${socket.id} left room ${roomName}`)
     } catch (error) {
       console.error('Error cancelling game:', error)
-      socket.emit('error', { message: 'Failed to cancel game' })
+      socket.emit('cancelGameError', { message: 'Failed to cancel game' });
     }
-  })
+  });
 }
 
-export default gameSocket;
+export default gameSocket
