57f261205288de6f0b933f00a2e5af756ac394a4
ravid4d <ravichaudharyd4d@gmail.com>
2025-12-04 18:20:29 +0530

    added socket for chating in game

--- sockets/chatsocket.js
+++ sockets/chatsocket.js
@@ -3,6 +3,30 @@ const chatSocket = (io, socket) => {
     console.log('Message:', msg)
     io.emit('chat message', msg) // Broadcast to all clients
   });
+
+  socket.on('gameSendMessage', data => {
+    try
+    {
+      const {gameId,userId,message} = data;
+      if(!gameId || !userId || !message)
+      {
+        return socket.emit("gameChatError", {
+          success : false,
+          message : "Missing parameters"
+        });
+      }
+
+      const roomName = `game_${gameId}`
+
+      socket.to(roomName).emit('gameRecievedMessage',{
+        gameId,userId,message,timestamp : new Date
+      });
+    }
+    catch(error)
+    {
+
+    }
+  });
 }
 
 export default chatSocket
